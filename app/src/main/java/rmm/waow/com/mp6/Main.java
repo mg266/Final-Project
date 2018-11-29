5a6102a4c88444b7bad3479193fdc8660c8e1756
Matthew Augustyn <ma62@illinois.edu>
2018-11-28 23:44:38 -0600

    New beginnings

---
+++ app/src/main/java/rmm/waow/com/mp6/Main.java
@@ -0,0 +1,11 @@
+package rmm.waow.com.mp6;
+
+/**
+ * todo better description
+ * This is where we begin.
+ */
+public class Main
+{
+    /** Hmmmmm... */
+    public static boolean projectStarted = true;
+}
