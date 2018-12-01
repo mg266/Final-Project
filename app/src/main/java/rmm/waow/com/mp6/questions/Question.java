482410dd207244f69a503e0241e8e7028f3401bb
Matthew Augustyn <ma62@illinois.edu>
2018-12-01 17:16:06 -0600

    added Question and Answer classes and an additional layout for displaying questions.

--- app/src/main/java/rmm/waow/com/mp6/questions/Question.java
+++ app/src/main/java/rmm/waow/com/mp6/questions/Question.java
@@ -1,5 +1,12 @@
 package rmm.waow.com.mp6.questions;
 
+import java.util.ArrayList;
+
+/**
+ * One single question that stores its flavor text and multiple answers.
+ */
 public class Question
 {
+    /** The possible answers to this question. */
+    private ArrayList<Answer> answers;
 }
