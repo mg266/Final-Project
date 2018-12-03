2efdc0dadd672d2245486b17d76bb02582733e41
Matthew Augustyn <ma62@illinois.edu>
2018-12-03 00:30:17 -0600

    Basics for the Wrapper class.

--- app/src/main/java/rmm/waow/com/mp6/Main.java
+++ app/src/main/java/rmm/waow/com/mp6/Main.java
@@ -1,19 +1,8 @@
 package rmm.waow.com.mp6;
 
-import android.util.Log;
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 
 import rmm.waow.com.mp6.questions.Question;
-
-import static com.android.volley.VolleyLog.TAG;
 
 /**
  * This is where the fun begins.
@@ -29,25 +18,6 @@ public class Main
      */
     public static void main(String unused[])
     {
-//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-//                Request.Method.GET,
-//                "https://pokeapi.co/api/v2/pokemon-species/1/",
-//                null,
-//                new Response.Listener<JSONObject>()
-//                {
-//                    @Override
-//                    public void onResponse(final JSONObject response)
-//                    {
-//                        Log.d(TAG, response.toString());
-//                    }
-//                }
-//                , new Response.ErrorListener()
-//                {
-//                    @Override
-//                    public void onErrorResponse(final VolleyError error)
-//                    {
-//                        Log.w(TAG, error.toString());
-//                    }
-//        });
+    
     }
 }
--- instant/src/main/java/com/waow/hmm/mp6/instant/MainActivity.java
+++ instant/src/main/java/com/waow/hmm/mp6/instant/MainActivity.java
@@ -3,8 +3,10 @@ package com.waow.hmm.mp6.instant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -13,14 +15,36 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button beginQuiz = (Button) findViewById(R.id.beginQuiz);
+        Button beginQuiz = findViewById(R.id.beginQuiz);
         beginQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivityQuestion();
             }
         });
-
+        
+        //todo implement this and make it call the Wrapper class that Matthew is making
+//        super.onCreate(savedInstanceState);
+//
+//        // Set up a queue for our Volley requests
+//        requestQueue = Volley.newRequestQueue(this);
+//
+//        // Load the main layout for our activity
+//        setContentView(R.layout.activity_main);
+//
+//        // Attach the handler to our UI button
+//        final Button startAPICall = findViewById(R.id.startAPICall);
+//        startAPICall.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(final View v) {
+//                Log.d(TAG, "Start API button clicked");
+//                startAPICall();
+//            }
+//        });
+//
+//        // Make sure that our progress bar isn't spinning and style it a bit
+//        ProgressBar progressBar = findViewById(R.id.progressBar);
+//        progressBar.setVisibility(View.INVISIBLE);
     }
     public void openActivityQuestion() {
         Intent openQuestion = new Intent(this, QuestionsActivity.class);
