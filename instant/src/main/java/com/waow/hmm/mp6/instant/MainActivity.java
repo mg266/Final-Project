2e3e74482622c38a362c1efbddcfd41d1a41229b
riya jain <rjain35@illinois.edu>
2018-12-02 21:43:24 -0600

    Got a basic UI together!!

--- instant/src/main/java/com/waow/hmm/mp6/instant/MainActivity.java
+++ instant/src/main/java/com/waow/hmm/mp6/instant/MainActivity.java
@@ -1,7 +1,10 @@
 package com.waow.hmm.mp6.instant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -9,7 +12,20 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question);
+        setContentView(R.layout.activity_main);
+        Button beginQuiz = (Button) findViewById(R.id.beginQuiz);
+        beginQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityQuestion();
+            }
+        });
 
+        //setContentView(R.layout.activity_question);
+
+    }
+    public void openActivityQuestion() {
+        Intent openQuestion = new Intent(this, QuestionsActivity.class);
+        startActivity(openQuestion);
     }
 }
