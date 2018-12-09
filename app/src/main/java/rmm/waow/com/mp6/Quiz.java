6fc2de339f592cd10a9c4ce2e7c7ea557dd6276a
Matthew Augustyn <ma62@illinois.edu>
2018-12-09 17:10:16 -0600

    -Added JSONObject constructor for Quiz

--- app/src/main/java/rmm/waow/com/mp6/Quiz.java
+++ app/src/main/java/rmm/waow/com/mp6/Quiz.java
@@ -1,18 +1,29 @@
 package rmm.waow.com.mp6;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import rmm.waow.com.mp6.questions.Question;
+import rmm.waow.com.mp6.scoring.Tally;
 
 /**
  * This is what stores all of the Questions, Tallies, and .
  */
 public class Quiz
 {
-    /** The list of every question in the quiz. */
+    /** The logging tag for this class. */
+    private static final String TAG = "Waow:Quiz";
+    /** The String name of the Questions in a JSON object describing a Quiz object. */
+    private static final String QUESTIONS = "questions";
+    /** The list of every Question in the quiz. */
     private ArrayList<Question> allQuestions;
+    /** The list of every Tally for this quiz. */
+    private ArrayList<Tally> allTallies;
     /** The app's data as read from appdata.json. */
     private JSONObject appData;
     
@@ -20,9 +31,28 @@ public class Quiz
      * JSON constructor, creates the Quiz from the appdata.json file.
      * @param initAppData the local quiz files, read only.
      */
-    public Quiz(JSONObject initAppData)
+    public Quiz(JSONObject initAppData) throws JSONException
     {
-        appData = initAppData;
-        //todo Matthew implement
+        JSONArray tempQuestions = initAppData.getJSONArray(QUESTIONS);
+        try
+        {
+            for (int i = 0; i < tempQuestions.length(); i++)
+            {
+                add(new Question(tempQuestions.getJSONObject(i)));
+            }
+        }
+        catch (JSONException e)
+        {
+            Log.d(TAG, "JSON Read error. Name: < questions >", e);
+        }
+    }
+    
+    /**
+     * Adds a Question to the Quiz.
+     * @param toAdd the Question being added
+     */
+    private void add(Question toAdd)
+    {
+        allQuestions.add(toAdd);
     }
 }
